53b12750d6315d3a1fc88ee710ff454b20bb7022
zhangys11 <oo@zju.edu.cn>
2022-04-20 18:27:54 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="wCLAMs",
     
     # Version number (initial):
-    version="0.1.1",
+    version="0.1.2",
     
     # Application author details:
     author="Zhang",
@@ -15,6 +15,8 @@ setup(
     
     # Packages
     packages=["wCLAMs"],
+
+    package_dir={'wCLAMs.templates': 'wCLAMs/templates', 'wCLAMs.static': 'wCLAMs/static'},
     
     # Include additional files into the package
     include_package_data=True,
@@ -26,11 +28,12 @@ setup(
     license="LICENSE.txt",
     description="A GUI wrapper for pyCLAMs. Rewritten using Flask.",
     
-    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+    # long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     
     # Dependent packages (distributions)
     install_requires=[
         "flask",
+        "pyCLAMs",
     ],
 )
 
